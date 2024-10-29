4a2aaacfee27fbc49811d168495b596ca7c6c7ec
erikacamilleri <erika.camilleri@gmail.com>
2024-10-29 09:47:43 +0100

    Answer IsItEven.java

--- chapters/src/chapter03/IsItEven.java
+++ chapters/src/chapter03/IsItEven.java
@@ -11,18 +11,27 @@ import java.util.Scanner;
 public class IsItEven {
     public static void main(String[] args) {
         
-        // TODO: Make a Scanner object and store it in input
+        // Make a Scanner object and store it in input
         // This helps to get a number from the user
         Scanner input = new Scanner(System.in);
 
         // Onboarding the user
         System.out.println("Welcome, this program checks whether your number is odd or even.");
         
-        // TODO: The number typed in by the user
+        // The number typed in by the user
         // is stored as an integer
         // Display an instruction to the user
+        System.out.println("Please enter a number: ");
+        int numberIn = input.nextInt();
         
-        // TODO: Use the mod operator to get remainder
+        // Use the mod operator to get remainder
+        int remainder = numberIn % 2;
+        // We are using == for equality or "is equal to"
+        if (remainder == 0) {
+            // Code to run when remainder is 0
+            System.out.println("Number is EVEN!");
+        }
+
         
         // We are good devs and close bulky resources
         input.close();
