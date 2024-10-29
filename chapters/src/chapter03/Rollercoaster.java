334336357f5276278181bb0b7be2ae52ffbf56d8
erikacamilleri <erika.camilleri@gmail.com>
2024-10-29 09:56:44 +0100

    Homework for practice Rollercoaster

---
+++ chapters/src/chapter03/Rollercoaster.java
@@ -0,0 +1,10 @@
+package chapter03;
+
+/**
+ * TODO: Homework
+ * Adapt the program in Java In Two Semesters Page 44
+ * So that the program checks for your height
+ */
+public class Rollercoaster {
+    
+}
