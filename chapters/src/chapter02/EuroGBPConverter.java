f34c2a543e0de1f6b3475303fa3c1baf91b2cc68
erikacamilleri <erika.camilleri@gmail.com>
2024-11-11 14:58:53 +0100

    We did this in an extra free lesson

--- chapters/src/chapter02/EuroGBPConverter.java
+++ chapters/src/chapter02/EuroGBPConverter.java
@@ -1,9 +1,22 @@
 package chapter02;
 
 /**
- * TODO: Are you up to the challenge?
+ * Are you up to the challenge?
+ * How will this program look like?
+ * 
+ * 10 Euros equals
+ * *** 8.33 Pound Sterling ***
  */
 public class EuroGBPConverter {
     public static void main(String[] args) {
+        // Declaring a variable called euros of data type double and we assigned it to 10.00
+        double euros = 10.00;
+        
+        // Declaring a variable called poundSterling of data type double and we assigned it to euros 
+        double poundSterling = 0.83 * euros;
+
+        // Display text
+        System.out.println(euros + " Euros equals");
+        System.out.println(poundSterling + " Pound sterling");
     }
 }
