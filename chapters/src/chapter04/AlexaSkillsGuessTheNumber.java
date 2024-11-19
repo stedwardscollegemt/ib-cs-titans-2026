ec17df5050c2c0544be5ce442a7c887753d24bbe
erikacamilleri <erika.camilleri@gmail.com>
2024-11-19 09:51:14 +0100

    AlexaSkillsGuessTheNumber CS Titans version

--- chapters/src/chapter04/AlexaSkillsGuessTheNumber.java
+++ chapters/src/chapter04/AlexaSkillsGuessTheNumber.java
@@ -1,9 +1,8 @@
 package chapter04;
 
 import java.util.Random;
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
-
-import helpers.Keyboard;
 
 public class AlexaSkillsGuessTheNumber {
     
@@ -12,13 +11,16 @@ public class AlexaSkillsGuessTheNumber {
         // on board the user and give instruction
         System.out.println("Hello there, let's play: Guess The Number.");
         System.out.println("Are you ready?");
-        String response = Keyboard.readString();
+
+        Scanner input = new Scanner(System.in);
+        char response = input.next().charAt(0);
 
         // check whether the user wants to play
-        if (response.equals("yes") || response.equals("y")) {
+        if (response == 'y') {
             System.out.println("Hmm... all right let me think of a number...");
 
             // get a random number using Random provided by Java
+            // the following code mimics rolling a dice and gives a number between 0 and 10
             int number = (new Random()).nextInt(10);
             
             // fake Alexa thinking
@@ -33,9 +35,9 @@ public class AlexaSkillsGuessTheNumber {
             // alert the user that they are going to start
             int triesLeft = 3;
             System.out.println("Ok. You have 3 tries left. Have a go at your first guess.");
-            int guess = Keyboard.readInt();
+            int guess = input.nextInt();
             
-            //
+            // guess not equal number AND triesLest is greater than 1
             while (guess != number && triesLeft > 1) {
                 if (guess < number) { // give hints after a wrong guess
                     System.out.println("You are not correct! The number I am thinking of, is larger. Take another guess...");
@@ -43,11 +45,12 @@ public class AlexaSkillsGuessTheNumber {
                     System.out.println("You are not correct! The number I am thinking of, is smaller. Take another guess...");
                 }
                 //
-                guess = Keyboard.readInt();
+                guess = input.nextInt();
+                // we make the problem smaller by decreasing the tries
                 triesLeft = triesLeft - 1;
             }
                
-            //
+            // triesLeft greater than 0 AND guess is equal to number
             if (triesLeft > 0 && guess == number) { // a congratulations message for a win
                 System.out.println("Well done, you guessed. That was a good round.");
             } else { // a consolation message for a loss and close off game
@@ -59,6 +62,8 @@ public class AlexaSkillsGuessTheNumber {
             System.out.println("That is all right. Maybe some other time...");
             System.out.println("Have a great day!");
         }
+
+        input.close();
     }
 
     // a method that will make the process idle
@@ -67,6 +72,6 @@ public class AlexaSkillsGuessTheNumber {
         try {
             TimeUnit.SECONDS.sleep(seconds);
         } catch (InterruptedException e) {
-        }
+    }
     }
 }
