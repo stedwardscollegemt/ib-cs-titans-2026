82883af7260fc52083062b364e6ce9a36a46f66a
erikacamilleri <erika.camilleri@gmail.com>
2024-11-19 09:24:09 +0100

    PrintFirst10EvenNumbers (While Loop)

--- chapters/src/chapter04/PrintFirst10EvenNumbers.java
+++ chapters/src/chapter04/PrintFirst10EvenNumbers.java
@@ -3,6 +3,21 @@ package chapter04;
 public class PrintFirst10EvenNumbers {
 
     public static void main(String[] args) {
+
+        // title
+        System.out.println("*** Print First 10 Even Numbers ***");
+        System.out.println("-----------------------------------");
         
+        // initialising our algorithm with the variables we need
+        int nextEvenNumber = 2;
+
+        // here is a while loop in Java
+        // it is very similar to an if statement
+        while (nextEvenNumber <= 20) {
+            // the action we want repeated
+            System.out.println("Next even number: " + nextEvenNumber);
+            // make my problem smaller
+            nextEvenNumber = nextEvenNumber + 2;
+        }
     }
 }
