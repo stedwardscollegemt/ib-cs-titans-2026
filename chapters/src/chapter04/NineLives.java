fc1a3e2e6ad98947a10f72b0a968c9028af0a52d
erikacamilleri <erika.camilleri@gmail.com>
2024-11-25 14:49:09 +0100

    We have a plan for programming NineLives

---
+++ chapters/src/chapter04/NineLives.java
@@ -0,0 +1,23 @@
+package chapter04;
+
+public class NineLives {
+
+    public static void main(String[] args) {
+
+        // TODO: Display a title for your program
+
+        // TODO: Variables we need (e.g., int lives, char guess_letter, String secret_word)
+
+        // TODO: Ask the user whether they want to play
+
+        // TODO: If they do want to play, then display the number of lives and the first hint e.g.,
+        //       Lives: 9       Hint: _ _ _ _ _
+
+        // TODO: While lives is greater than 0
+            // TODO: Ask the user to guess a letter
+
+            // TODO: Check whether the guess is correct, 
+            //       If it is then update hint
+            //       Else update lives
+    }
+}
