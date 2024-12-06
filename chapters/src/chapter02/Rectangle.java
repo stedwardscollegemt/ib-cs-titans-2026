e3476271e83c7bdf0092ed58c9e6de0cc6aa1f8e
czarkov <czarkov@stedwards.edu.mt>
2024-12-06 11:07:51 +0100

    Testing Comments and Commits

--- chapters/src/chapter02/Rectangle.java
+++ chapters/src/chapter02/Rectangle.java
@@ -8,6 +8,7 @@ public class Rectangle {
     public static void main(String[] args) {
         
         // TODO: Display an explanation for Rectangle program
+        System.out.println("We are great at doing rectangles");
 
         // TODO: Create variables of type double for w, l
 
