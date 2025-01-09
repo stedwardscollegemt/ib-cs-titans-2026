f6259f8c1d7c096c9794db33e895a7f03f6e3b6d
czarkov <czarkov@stedwards.edu.mt>
2025-01-09 13:48:43 +0100

    Dice roll sub
    Added Dice Roller Sub

---
+++ chapters/src/chapter02/Roller.java
@@ -0,0 +1,48 @@
+package chapter02;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Roller {
+
+public static int rollDice()
+{
+    Random rand = new Random();
+    int roll = rand.nextInt(6) + 1;
+    return roll;
+}
+
+public static void main(String[] args) {
+    //Display welcome message 
+    System.out.println("Calling a Math Function...");
+
+    //Scanner keyIn = new Scanner(System.in);
+    //Scanner userInput;
+    //System.out.print("Press the ENTER key to roll the dice.");
+    //keyIn.nextLine();
+
+    int tries = 1;
+
+    int sum = 0;
+
+    while (sum <= 10 && sum != 6) 
+    
+    {
+    // Here is where you call your newly created method
+    int roll1 = rollDice();
+
+    int roll2 = rollDice();
+
+    sum = roll1 + roll2;
+    
+    //tries = tries + 1;
+
+    System.out.println(roll1 + " + " + roll2 + " = " + sum + ", Try number: "  + tries);
+
+    tries++;
+    }
+}
+
+
+
+}
