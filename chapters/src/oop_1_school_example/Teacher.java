6691ac9e2ec73b761fb70bfd0f81b24536556fe7
erikacamilleri <erika.camilleri@gmail.com>
2025-03-25 09:51:23 +0100

    Nature of objects as data structures

---
+++ chapters/src/oop_1_school_example/Teacher.java
@@ -0,0 +1,19 @@
+package oop_1_school_example;
+
+public class Teacher {
+
+    /* ----------------  DATA -------------------------- */
+    // Declare your variables for Teacher
+    int id;
+    
+    String name;
+
+
+
+
+
+
+
+    /* ----------------  BEHAVIOUR -------------------------- */
+    
+}
