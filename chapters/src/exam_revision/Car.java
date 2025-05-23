56e00525b10b9e8104236cea83984ba2346621a5
erikacamilleri <erika.camilleri@gmail.com>
2025-05-23 10:58:30 +0200

    Mapped more assessment criteria, cleaned up comments for better study.

--- chapters/src/exam_revision/Car.java
+++ chapters/src/exam_revision/Car.java
@@ -1,14 +1,18 @@
 package exam_revision;
 
 // D.1.1 - Outline the nature of an object
+// D.3.1 Program Development, Define: class
+// D.3.5 Program Development, Construct code related to object-oriented fundamentals
 // Grouping of DATA (variables) with BEHAVIOUR (methods)
 public class Car {
 
     // Data | State (member variables/attributes/properties)
     // Describe what the car is made up of
-    int mileage;
+    
+    // D.1.9 Explain the need for different data types to represent real world data items
+    int mileage; // a number we can perform operations on (like line 18)
 
-    boolean isSecondHand;
+    boolean isSecondHand; // a Yes/No flag which requires little validation since it is so simple
 
     // Behaviour (methods)
     // Describe what the car can do
--- chapters/src/exam_revision/CarFactory.java
+++ chapters/src/exam_revision/CarFactory.java
@@ -1,17 +1,18 @@
 package exam_revision;
 
+// D.3.5 Program Development, Construct code related to object-oriented fundamentals
 public class CarFactory {
     
     public static void main(String[] args) {
-        // Creating an *instance* of Car
+
+        // D.3.1 Program Development, Define: instance
         // Instances take up memory and have a state, i.e., attributes are filled with values,
         // and the state can be changed by invoking the behaviour, i.e., calling the methods
 
-        // Instance one
+        // Creating an *instance* of Car
         Car carA = new Car();
         carA.mileage = 50;
         carA.isSecondHand = false;
         carA.drive(10);
-
     }
 }
