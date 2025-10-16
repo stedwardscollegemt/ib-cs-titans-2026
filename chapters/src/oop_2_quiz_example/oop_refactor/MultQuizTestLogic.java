3f379ad343c0cb24dcb14a13b35da95e746c3712
erikacamilleri <erika.camilleri@gmail.com>
2025-10-16 14:40:50 +0200

    Refactor and unit test

--- chapters/src/oop_2_quiz_example/oop_refactor/MultQuizTestLogic.java
+++ chapters/src/oop_2_quiz_example/oop_refactor/MultQuizTestLogic.java
@@ -1,5 +1,30 @@
 package oop_2_quiz_example.oop_refactor;
 
 public class MultQuizTestLogic {
+
+    /**
+     * --- STATE: Declare Member Variables
+     */
+    String[] questions;
     
+    int[] answers;
+
+    int score;
+
+    /**
+     * --- BEHAVIOUR: Methods
+     */
+    public void initTest(int mulTable) {
+        for(int i = 0; i < questions.length; i++) {
+            // generate the question e.g., 1 X 5 =
+            answers[i] = (i + 1) * mulTable;
+            questions[i] = "" + (i + 1) + " X " + mulTable + "=";
+        }
+    }
+
+    public void updateScore(int questionIndex, int userAnswer) {
+        if (userAnswer == answers[questionIndex]) {
+            score = score + 1;
+        }
+    }
 }
--- chapters/src/oop_2_quiz_example/oop_refactor/MultQuizUI.java
+++ chapters/src/oop_2_quiz_example/oop_refactor/MultQuizUI.java
@@ -1,5 +1,7 @@
 package oop_2_quiz_example.oop_refactor;
 
+import helpers.Keyboard;
+
 public class MultQuizUI {
 
     public static void main(String[] args) {
@@ -7,12 +9,22 @@ public class MultQuizUI {
 
         // create an instance of a MuliQuizTestLogic
         MultQuizTestLogic test0 = new MultQuizTestLogic();
-        MultQuizTestLogic test1 = new MultQuizTestLogic();
-        MultQuizTestLogic test2 = new MultQuizTestLogic();
-        MultQuizTestLogic test3 = new MultQuizTestLogic();
-        MultQuizTestLogic test4 = new MultQuizTestLogic();
 
+        test0.initTest(5);
+        
+        // loop through the questions to test the student
+        for (int i = 0; i < test0.questions.length; i++) {
+            System.out.println(test0.questions[i]);
+            int userAnswer = Keyboard.readInt(); 
+            // how do I check that the userAnswer is correct?
+            test0.updateScore(i, userAnswer);
+        }
 
+        if (test0.score == 5) {
+            System.out.println("Wow, full marks!");
+        } else {
+            System.out.println("You need a bit more practice.");
+        }
     }
     
 }
